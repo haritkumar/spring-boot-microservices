a8a3fac5fe32cf12319cfd7e4f51317f7533a55d
Kumar, Harit <harit_kumar@optum.com>
2020-11-08 00:19:03 +0530

    common swagger doc

---
+++ documentation/src/main/java/com/movies/store/controller/ServiceDefinitionController.java
@@ -0,0 +1,29 @@
+package com.movies.store.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.movies.store.swagger.ServiceDefinitionsContext;
+
+
+/**
+ * 
+ * <pre>
+ *  Controller to serve the JSON from our in-memory store. So that UI can render the API-Documentation	
+ * </pre>
+ */
+@RestController
+public class ServiceDefinitionController {
+	
+	@Autowired
+	private ServiceDefinitionsContext definitionContext;
+	
+	@GetMapping("/service/{servicename}")
+	public String getServiceDefinition(@PathVariable("servicename") String serviceName){
+		
+		return definitionContext.getSwaggerDefinition(serviceName);
+		
+	}
+}
--- ws-movies/src/main/java/com/movies/store/controller/Api.java
+++ ws-movies/src/main/java/com/movies/store/controller/Api.java
@@ -1,4 +1,4 @@
-package com.movies.store;
+package com.movies.store.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
